f1376c88a92532a327d10d2b22c011f782a2d9e6
Dawsjames <dawsonjamesvmarcos@gmail.com>
2025-08-15 21:17:46 +0800

    page animation

--- src/hooks/useScrollAnimation.js
+++ src/hooks/useScrollAnimation.js
@@ -1,17 +1,17 @@
-import { useEffect, useRef } from 'react'
-import { useInView } from 'react-intersection-observer'
+import { useEffect, useRef } from "react"
+import { useInView } from "react-intersection-observer"
 
 export const useScrollAnimation = (options = {}) => {
   const {
     threshold = 0.1,
     triggerOnce = true,
-    rootMargin = '0px 0px -100px 0px'
+    rootMargin = "0px 0px -100px 0px",
   } = options
 
   const { ref, inView } = useInView({
     threshold,
     triggerOnce,
-    rootMargin
+    rootMargin,
   })
 
   return { ref, inView }
